7bd9919e7723faea00e94b522fcaa855d455acf9
Fábio <fabiomatavelli@gmail.com>
2019-01-20 13:07:59 -0200

    Solution for 727

---
+++ problems/727-remove-duplicates-from-sorted-array/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_table = {}
+        i = 0
+        while i < len(nums):
+          if not hash_table.has_key(nums[i]):
+            hash_table[nums[i]] = True
+            i += 1
+          else:
+            nums.pop(i)
+
+        return len(nums)
