66790279485b3081f0e6b8e380804cd88c24d703
Fábio <fabiomatavelli@gmail.com>
2019-01-20 16:58:00 -0200

    Add test solution for two sum

---
+++ problems/546-two-sum/test_solution.py
@@ -0,0 +1,28 @@
+import worst_solution
+import best_solution
+
+class Case(object):
+  def __init__(self, nums, target, result):
+    self.nums = nums
+    self.target = target
+    self.result = result
+
+tests = [
+  Case([2,7,11,15], 9, [0, 1]),
+  Case([5,2,3,10,4,8], 12, [1, 3]),
+  Case([2,3,6,7,4,1], 5, [0, 1]),
+  Case([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 39, [19, 20]),
+  Case([3,2,4], 6, [1, 2])
+]
+
+def test_worst_solution():
+  s = worst_solution.Solution()
+  for t in tests:
+    result = s.twoSum(t.nums, t.target)
+    assert  result == t.result
+
+def test_best_solution():
+  s = best_solution.Solution()
+  for t in tests:
+    result = s.twoSum(t.nums, t.target)
+    assert  result == t.result
