0a91dab0a98e73c5dde89d4658ce95fd7beab5fd
Fábio <fabiomatavelli@gmail.com>
2019-01-20 17:03:14 -0200

    Add test to 727

---
+++ problems/727-remove-duplicates-from-sorted-array/test_727_solution.py
@@ -0,0 +1,17 @@
+import solution
+
+class Case(object):
+  def __init__(self, nums, result):
+    self.nums = nums
+    self.result = result
+
+tests = [
+  Case([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8], 9),
+  Case([0, 1, 2, 3, 4, 5], 6),
+  Case([], 0)
+]
+
+def test_solution():
+  s = solution.Solution()
+  for t in tests:
+    assert  s.removeDuplicates(t.nums) == t.result
